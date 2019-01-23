0c231748e2dc2b745455e7606c7e6b60dfde2760
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-01-23 19:32:47 +0530

    setup twitter login

--- config/config.js
+++ config/config.js
@@ -3,4 +3,9 @@ module.exports = {
     mongodb: {
         dbURI: "mongodb://iliyas137:atrium20@ds149344.mlab.com:49344/learning"
     }
+    TwitterKeys: {
+        consumer_key: "uEOUXLwpDpsCJH297f0lyKpm8",
+        consumer_secret: "NbpXrmuDHyZ85haF7EfWgkGwcaig4S3VJmCurfVhQzF5StAdsr",
+
+    }
 }
--- server.js
+++ server.js
@@ -1,29 +1,54 @@
 const express = require("express")
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
-const mongoURL = require("./config/config").mongodb.dbURI
 const mongoose = require("mongoose")
+const morgan = require("morgan")
+const twitterStatergy = require("passport-twitter").Statergy
+
+
+const apiController = require("./controller/apiController")
+const twitterKeys = require("./config/config").twitterKeys
+const mongoURL = require("./config/config").mongodb.dbURI
+
 
 const app = express()
 const port = process.env.PORT || 3000
 
+
 app.set("views", "./views")
 app.set("view engine", "ejs")
 
+passport.use(new twitterStatergy({
+    consumerKey: twitterKeys.consumer_key
+    consumerSecret: twitterKeys.consumer_secret
+    callbackURL: "https://quiet-scrubland-22973.herokuapp.com/login/auth/twitter/callback"
+}))
+
+app.use(cors())
 app.use(express.static(__dirname + "/public"))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(morgan("combined"))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 mongoose.promise = global.promise
 mongoose.connect(mongoURL, { useNewUrlParser: true }, () => {
     console.log("successfully connected to mongodb")
 })
 
-
+apiController(app)
 app.get("/", (req, res) => {
-    res.sendFile("index")
+    res.render("login")
 })
 
+app.get("/login/auth/twitter", passport.authenticate('twitter'))
+
+app.get("/login/auth/twitter/callback", passport.authenticate('twitter', { failureRedirect: '/login' }), (req, res) => {
+    res.redirect("/profile")
+})
 app.listen(port, () => {
     console.log(`server is listening at ${port}`)
 })
