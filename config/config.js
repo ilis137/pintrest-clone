c01d6c7771a37fc8b5fdbdda842fd6162e942ca6
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-01-24 08:43:14 +0530

    update twitter keys

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ module.exports = {
 
     mongodb: {
         dbURI: "mongodb://iliyas137:atrium20@ds149344.mlab.com:49344/learning"
-    }
+    },
     TwitterKeys: {
-        consumer_key: "uEOUXLwpDpsCJH297f0lyKpm8",
-        consumer_secret: "NbpXrmuDHyZ85haF7EfWgkGwcaig4S3VJmCurfVhQzF5StAdsr",
+        consumer_key: "lYs48RIVtfH33I71zCUV86GGw",
+        consumer_secret: "I0Y5DSPzPqnRfDAdpyOLuaYhZ5hp0FGMJRTAujMwmSnlmw8bbr",
 
     }
 }
