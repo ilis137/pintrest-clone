12380214a2e6202f91d2398b77a8e1ca8f555b5a
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-01-30 07:34:14 +0530

    log request

--- controller/apiController.js
+++ controller/apiController.js
@@ -41,7 +41,7 @@ module.exports = (app) => {
     })
 
     app.post("/like", (req, res) => {
-        console.log(req.query)
+        console.log(req)
         Image.findById(req.body.id).then((elem) => {
             if (elem.likedBy.includes(req.user.username)) {
 
